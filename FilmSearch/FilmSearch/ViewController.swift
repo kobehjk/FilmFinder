6d7e408e4cd3a3c89dda7454783b4586ab9b2473
kobehjk <hejinkun0808@163.com>
2017-12-28 19:47:33 +0800

    Init

---
+++ FilmSearch/FilmSearch/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FilmSearch
+//
+//  Created by hjk on 2017/12/28.
+//  Copyright © 2017年 kobehjk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
